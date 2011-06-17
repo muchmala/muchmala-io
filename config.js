274bf530ce76e4acc827a7622c0114018e06d8c2
Taras Kunch <taras.kunch@gmail.com>
2011-06-17 12:44:37 +0300

    Refactored config. Added functionality to inform other nodes about starting of new io-server. Incremented version.

--- config.js
+++ config.js
@@ -1,54 +1,23 @@
 var config = exports;
-config.DEV = true;
 
-config.HTTP_HOST = '0.0.0.0';
-config.HTTP_PORT = 80;
+config.io = {
+    externalHost: 'io.muchmala.dev',
+    externalPort: 80,
+    internalHost: '0.0.0.0',
+    internalPort: null
+};
 
-config.MAIN_URL   = 'http://muchmala.dev';
+config.queue = {
+    host: "127.0.0.1",
+    port: 6379,
+    password: undefined,
+    database: 0
+};
 
-config.STATIC_HOST = 'static.muchmala.dev';
-config.STATIC_PORT = 8080;
-config.STATIC_URL = 'http://' + config.STATIC_HOST + ':' + config.STATIC_PORT;
+var localConfigPath = './config.local.js';
+if (require('path').existsSync(localConfigPath)) {
+    var localConfig = require(localConfigPath),
+        deepExtend = require('muchmala-common').misc.deepExtend;
 
-config.MONGODB_HOST     = '127.0.0.1';
-config.MONGODB_USER     = 'mongodb';
-config.MONGODB_DATABASE = 'muchmala';
-
-config.REDIS_HOST       = '127.0.0.1';
-config.REDIS_PORT       = 6379;
-config.REDIS_PASSWORD   = undefined;
-config.REDIS_DATABASE   = 0;
-
-config.AWS_KEY          = null;
-config.AWS_SECRET       = null;
-config.S3_BUCKET_MAIN   = 'dev.muchmala.com';
-config.S3_BUCKET_STATIC = 'static.dev.muchmala.com';
-
-config.UTILS_DB = __dirname + '/utils.db';
-
-config.TWITTER_KEY      = '';
-config.TWITTER_SECRET   = '';
-config.FACEBOOK_ID      = '';
-config.FACEBOOK_SECRET  = '';
-config.GOOGLE_KEY       = '';
-config.GOOGLE_SECRET    = '';
-config.YAHOO_KEY        = '';
-config.YAHOO_SECRET     = '';
-
-config.FRONTEND_SERVERS = [
-    {externalHost: 'muchmala.dev', externalPort: 80, internalPort: 8081} //can be only one for now
-];
-
-config.IO_SERVERS = [
-    {externalHost: 'io1.muchmala.dev', externalPort: 80, internalPort: 8082},
-    {externalHost: 'io2.muchmala.dev', externalPort: 80, internalPort: 8083}
-];
-
-config.APP_SERVERS_COUNT = 1; //can be only one for now
-
-try {
-    var config_local = require('./config.local.js');
-    for (var key in config_local) {
-         config[key] = config_local[key];
-    }
-}catch(e) {}
+    deepExtend(config, localConfig);
+}
--- index.js
+++ index.js
@@ -1,23 +1,66 @@
-var ioCluster = require('socket.io-cluster');
-var config = require('./config');
-var http = require('http');
-var opts = require('opts');
+var http = require('http'),
+    opts = require('opts'),
+
+    ioCluster = require('socket.io-cluster'),
+    queueAdapter = require('muchmala-common').queueAdapter,
+
+    config = require('./config');
+
+var queue = new queueAdapter(config.queue);
 
 opts.parse([
     {
+        'short': 'H',
+        'long': 'externalHost',
+        'description': 'External HTTP host',
+        'value': true,
+        'required': false
+    }, {
+        'short': 'h',
+        'long': 'internalHost',
+        'description': 'Internal HTTP host',
+        'value': true,
+        'required': false
+    }, {
+        'short': 'P',
+        'long': 'externalPort',
+        'description': 'External HTTP port',
+        'value': true,
+        'required': false
+    }, {
         'short': 'p',
-        'long': 'port',
-        'description': 'HTTP port',
+        'long': 'internalPort',
+        'description': 'Internal HTTP port',
         'value': true,
         'required': false
     }
 ], true);
 
-var server = http.createServer();
-var ioNode = ioCluster.makeIoListener(server, config);
-var port = opts.get('port') || config.HTTP_PORT;
+var localConfig = {
+    externalHost: opts.get('externalHost') || config.io.externalHost,
+    externalPort: opts.get('externalPort') || config.io.externalPort,
+    internalHost: opts.get('internalHost') || config.io.internalHost,
+    internalPort: opts.get('internalPort') || config.io.internalPort
+};
 
-server.listen(port, config.HTTP_HOST);
+var host = localConfig.internalHost || localConfig.externalHost;
+var port = localConfig.internalPort || localConfig.externalPort;
+
+console.log(localConfig, host, port)
+var server = http.createServer();
+var ioNode = ioCluster.makeIoListener(server, {
+        REDIS_HOST: config.queue.host,
+        REDIS_PORT: config.queue.port,
+        REDIS_PASSWORD: config.queue.password,
+        REDIS_DATABASE: config.queue.database
+    });
+
+server.listen(port, host, function() {
+    queue.broadcast('servers', {
+            type: 'io',
+            interface: localConfig
+        });
+});
 
 ioNode.getClientInfo = function(client) {
     return {
